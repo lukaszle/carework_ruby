d606485ae59eff1b3bdd956ebc4d4cf355ff2bd2
Prelang Builder <builder@prelang.com>
2016-11-29 13:58:36 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Klient' to allow for create, update

--- app/admin/klient.rb
+++ app/admin/klient.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Klient do
+  permit_params :imie, :nazwisko, :kid, :blad, :id
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
